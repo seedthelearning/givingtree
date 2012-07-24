d49d2568eec75afdc367cd96342458f46f57132e
ADigitalNative <adigitalnative@gmail.com>
2012-07-24 16:49:30 -0400

    Fixing helper to give amounts in dollars for total

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,7 +6,7 @@ module UsersHelper
 
   def seed_payout_in_dollars(seed_id)
     response = SCOOP_CLIENT.get_seed(seed_id)
-    response[:donation][:payout_cents]/100
+    convert_cents_to_dollars(response[:donation][:payout_cents])
   end
 
   def seed_participant_count(seed_id)
@@ -16,11 +16,11 @@ module UsersHelper
 
   def seed_total_raised(seed_id)
     response = SCOOP_CLIENT.get_seed(seed_id)
-    response[:donation][:total_donated]
+    convert_cents_to_dollars(response[:donation][:total_donated])
   end
 
   def convert_cents_to_dollars(amount_cents)
-    amount_cents / 100
+    amount_cents/100
   end
 
 end
