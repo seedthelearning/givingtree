7068313712c89b1840d22b1cfd910fc0e373d023
ADigitalNative <adigitalnative@gmail.com>
2012-07-24 14:12:56 -0400

    Added basic text for alerts, may need reworking of actual messages

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -5,8 +5,10 @@ class ParticipantsController < ApplicationController
     seed_id = params[:id]
     if current_user
       create_participant_and_render(seed_id)
+      flash[:alert] = "You've unlocked $1. Why not plant your own seed?"
     else
       session[:seed_id] = seed_id
+      flash[:alert] = "Almost there! Log in to unlock $1 and help seed the learning."
       authenticate_user!
     end
   end
@@ -14,6 +16,7 @@ class ParticipantsController < ApplicationController
   def create_from_session  
     seed_id = session.delete(:seed_id)
     create_participant_and_render(seed_id)
+    flash[:alert] = "You've unlocked $1. Why not plant your own seed?"
   end
 
 private
