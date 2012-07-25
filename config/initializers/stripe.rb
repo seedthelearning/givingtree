5993263a0fbcd5c89baab88a0a34a2a10bc489e9
Jonan Scheffler <jonanscheffler@gmail.com>
2012-07-25 04:10:38 -0400

    Stripe engaged

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,3 +1,9 @@
 require './config/secret/stripe_keys'
 
-Stripe.api_key = STRIPE_TEST_SECRET
+if Rails.env == 'production'
+  Stripe.api_key = STRIPE_LIVE_SECRET
+  STRIPE_PUBLIC = STRIPE_LIVE_PUBLIC
+else
+  Stripe.api_key = STRIPE_TEST_SECRET
+  STRIPE_PUBLIC = STRIPE_TEST_PUBLIC
+end
