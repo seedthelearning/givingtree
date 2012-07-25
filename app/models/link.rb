8a62f47004e22348a52182a1fb46f88d4805a9c1
Jonan Scheffler <jonanscheffler@gmail.com>
2012-07-25 17:03:53 -0400

    Apostrophe for Kids

--- app/models/link.rb
+++ app/models/link.rb
@@ -14,7 +14,7 @@ app_id=100736836740408&
 link=#{HOST_URL}/participants/#{text}&
 picture=http://1337807.com/images/seedthelearning.png&
 name=Seed%20The%20Learning&
-caption=Change%20Kids%20Lives&
+caption=Change%20Kids%27%20Lives&
 description=Visit%20SeedTheLearning.com%20and%20I%20will%20donate%20$1%20to%20DonorsChoose.org!%20Easiest%20Karma%20ever.&
 redirect_uri=http://seedthelearning.com/users/#{self.user_id}
     url
