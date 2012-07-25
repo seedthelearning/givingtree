9ac88486afe47510be2d0f5022438cc3cbf100cd
Jonan Scheffler <jonanscheffler@gmail.com>
2012-07-25 04:49:56 -0400

    Don't blow up if there isn't a donation

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -10,7 +10,7 @@ module UsersHelper
 
   def seed_payout_in_dollars(seed_id)
     response = SCOOP_CLIENT.get_seed(seed_id)
-    convert_cents_to_dollars(response[:donation][:payout_cents])
+    convert_cents_to_dollars(response[:donation][:payout_cents]) if response[:donation]
   end
 
   def seed_participant_count(seed_id)
@@ -20,7 +20,7 @@ module UsersHelper
 
   def seed_total_raised(seed_id)
     response = SCOOP_CLIENT.get_seed(seed_id)
-    convert_cents_to_dollars(response[:donation][:total_donated])
+    convert_cents_to_dollars(response[:donation][:total_donated]) if response[:donation]
   end
 
   def convert_cents_to_dollars(amount_cents)
