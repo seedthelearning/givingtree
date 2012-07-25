ec569056dde81e08a6898392198942f53890b4e6
Jonan Scheffler <jonanscheffler@gmail.com>
2012-07-25 16:08:12 -0400

    Adjust test for api/trees

--- spec/requests/trees_constroller_spec.rb
+++ spec/requests/trees_constroller_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
-describe TreesController do
+describe Api::TreesController do
   it "should return a tree" do
     id = "abcdef"
 
     SCOOP_CLIENT.should_receive(:get_tree).with(id)
 
-    get "/trees/#{id}"
+    get "/api/trees/#{id}"
   end
 end
