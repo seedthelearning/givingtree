ce3e007a9267d80f41bbb473a22395c2f3cfd945
Jonan Scheffler <jonanscheffler@gmail.com>
2012-07-24 21:16:33 -0400

    Style changes

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -8,24 +8,23 @@ class ParticipantsController < ApplicationController
     if current_user
       create_participant_and_render(seed_id)
     else
-      flash[:alert] = "Almost there! Log in to unlock $1 and help seed the learning."
+      flash[:alert] = "Almost there! Sign in and we'll give $1 to DonorsChoose.org."
       authenticate_user!
     end
   end
 
-  def create_from_session  
+  def create_from_session
     create_participant_and_render(session[:seed_id])
   end
 
 private
-  
+
   def create_participant_and_render(seed_id)
     response = create_participant(current_user.id, seed_id)
     if response[:status] == 201
-      flash[:alert] = "You've unlocked $1. Why not plant your own seed?"
+      flash[:alert] = "Thank you for valuing education! Keep the tree growing and plant your own seed."
     else
-      flash[:alert] = "Sorry you've already unlocked a donation from this seed. 
-                       Why not plant your own seed?"
+      flash[:alert] = "Sorry you've already unlocked a donation from this seed. Why not plant your own seed?"
     end
     redirect_to root_path
   end
