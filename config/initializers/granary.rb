93712abc85465d0bd76c5848b9c3d6ec1b564b92
Jonan Scheffler <jonanscheffler@gmail.com>
2012-07-25 02:44:01 -0400

    ROBOT HEROKU ATTACK

--- config/initializers/granary.rb
+++ config/initializers/granary.rb
@@ -1,5 +1,5 @@
 if Rails.env == 'production'
-  GRANARY_URL = "http://50.116.37.154:8080/granary/api/v1/"
+  GRANARY_URL = "http://seedthelearning.herokuapp.com/api/v1/"
 else
   GRANARY_URL = "http://localhost:3001/api/v1/"
 end
